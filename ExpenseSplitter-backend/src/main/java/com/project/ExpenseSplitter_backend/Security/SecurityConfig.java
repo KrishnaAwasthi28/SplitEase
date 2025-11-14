99521f5d1f7f613b94a9b145608026fa628807ec
Krishna Awasthi <krishhawasthi28@gmail.com>
2025-11-15 02:34:36 +0530

    added frontend url

--- ExpenseSplitter-backend/src/main/java/com/project/ExpenseSplitter_backend/Security/SecurityConfig.java
+++ ExpenseSplitter-backend/src/main/java/com/project/ExpenseSplitter_backend/Security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://your-frontend-domain.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://split-ease-puce.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
